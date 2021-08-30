87c2a2779e6aa82df7d69fdbb6ae740dfc65e29b
Oleg Derevyanko <derevyanko@MacBook-Pro-de-Oleg.local>
2021-08-30 14:56:04 +0300

    #4 App Meditation. #1. Add font files and color extensions.

---
+++ Meditation/Meditation/Extensions/Colors.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+extension Color {
+    public static var deepBlue: Color {
+        Color(red: 6/255, green: 22/255, blue: 76/255)
+    }
+    
+    public static var purple500: Color {
+        Color(red: 255/255, green: 98/255, blue: 0/255)
+    }
+    
+    public static var purple700: Color {
+        Color(red: 255/255, green: 55/255, blue: 0/255)
+    }
+    
+    public static var teal200: Color {
+        Color(red: 255/255, green: 3/255, blue: 218/255)
+    }
+    
+    public static var teal700: Color {
+        Color(red: 255/255, green: 1/255, blue: 135/255)
+    }
+}
